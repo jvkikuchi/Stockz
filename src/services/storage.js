6e9afcca8169f5034531bc3066feb330fd093b93
Gabriel Araújo Freitas <68439071+FreitasGa@users.noreply.github.com>
2022-04-26 08:33:45 -0300

    Update storage.js

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,26 +1,40 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://localhost:3000/items'
+const baseUrl = "http://localhost:3000/items";
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const { data } = await axios.get(baseUrl);
+  return data;
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const getOne = async (id) => {
+  const { data } = await axios.get(`${baseUrl}/${id}`);
+  return data;
+};
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+const create = async (newObject) => {
+  const { data } = await axios.post(baseUrl, newObject);
+  return data;
+};
 
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
-}
+const update = async (id, newObject) => {
+  const { data } = await axios.put(`${baseUrl}/${id}`, newObject);
+  return data;
+};
 
+const remove = async (id) => {
+  const { data } = await axios.delete(`${baseUrl}/${id}`);
+  return data;
+};
 
-export default {getAll, create, remove, update}
+const updateQuantity = async (id, newQuantity) => {
+  const object = await getOne(id);
+  const newObject = {
+    ...object,
+    quantity: newQuantity,
+  };
+
+  return await update(id, newObject);
+};
+
+export default { getAll, getOne, create, remove, update, updateQuantity };
